e954c9f84e864a854502893841a3437ede17722f
Yash0510 <yash.mishra@sparkeighteen.com>
2022-04-28 19:06:45 +0530

    TODOS update after feedback from demo session

--- 28-April-2022/To_Do_List/backup_todo.py
+++ 28-April-2022/To_Do_List/backup_todo.py
@@ -1,14 +1,31 @@
 from datetime import datetime
 
+
 class To_do_Item(object):
+    """
+    Creating class To_do_Item consisting of title,notes and time stamps which can interact with
+    each of these attributes.
+
+    """
 
     def __init__(self):
+        """
+        class To_do_Item constructor
+        Attributes : Title, Type: String
+                    Items(Notes), Type: List
+                    Time Stamps, Type: Date
+        """
         self.title = ''
-        self.items = [-1]
+        self.items = [-1]                   # -1, used as a placeholder
         self.created_at = datetime.now()
         self.modified_at = datetime.now()
 
     def _get_list_data(self, notes=None):
+        """
+
+        @param notes: The length of the list containing notes
+        @return: A string containing contents of the list in good form.
+        """
         if not notes:
             notes = range(1, len(self.items))
         list_data = str()
@@ -18,6 +35,10 @@ class To_do_Item(object):
         return list_data
 
     def __str__(self):
+        """
+
+        @return: the string when the instance of this class is passed as an argument to print function.
+        """
         return f"""
 ********************************************************************************
 Title: {str(self.title)}
@@ -25,25 +46,62 @@ ToDos:\n{'' if not self.items else self._get_list_data()}
 Created at:{self.created_at}\t\t\tModified at:{self.modified_at}
 ********************************************************************************
 """
-    def add_title(self,title):
-        self.title += title
 
-    def add_notes(self,item):
+    def add_title(self, title):
+        """
+
+        @param title: The title of the To_Do-Item. Type = String
+        @return: Title. Type = String
+        """
+        self.title += title
+        return self.title
+
+    def add_notes(self, item):
+        """
+
+        @param item: The item to be added to items list.
+        @return: String containing To_do item details including title,content and modified date time stamp.
+        """
         self.items.append(item)
-        self.created_at = datetime.now()
+        self.created_at = datetime.date()
         print(f"{item} added at {self.created_at}")
+        return f"""
+        ********************************************************************************
+        Title: {str(self.title)}
+        ToDos:\n{'' if not self.items else self._get_list_data()}
+        Created at:{self.created_at}\t\t\t
+        ********************************************************************************
+        """
 
     def __setitem__(self, key, edit):
+        """
+        Getter Method
+        @param key: The list index whose content is to be edited. Type: Integer
+        @param edit: The item which will be used to replace the present item. type: String
+        @return: String containing To_do item details including title,content and modified date time stamp.
+        """
         self.key = key
         self.edit = edit
         if self.key not in range(1, len(self.items)):
             print('Sorry, No existing notes here to edit')
         else:
             self.items[self.key] = self.edit
-            self.modified_at = datetime.now()
+            self.modified_at = datetime.today()
             print(f"{self.edit} added at {self.modified_at}")
+        return f"""
+        ********************************************************************************
+        Title: {str(self.title)}
+        ToDos:\n{'' if not self.items else self._get_list_data()}
+                                                \t\t\tModified at:{self.modified_at}
+        ********************************************************************************
+        """
 
     def __delitem__(self, index):
+        """
+
+        @param index:  The index of the list,the content of which will be deleted.
+        @return: None
+        """
         self.index = index
         if self.index not in range(1, len(self.items)):
             print(f"Sorry, No item found")
@@ -51,8 +109,12 @@ Created at:{self.created_at}\t\t\tModified at:{self.modified_at}
             print(f"{self.items[self.index]} deleted")
             del self.items[self.index]
 
-
     def search(self, query):
+        """
+
+        @param query: The item which to search in notes
+        @return: None
+        """
         for i, item in enumerate(self.items):
             if query in self.items[item]:
                 print(f'{query} found in {self.items[item]}')
@@ -61,62 +123,71 @@ Created at:{self.created_at}\t\t\tModified at:{self.modified_at}
             print("Sorry, your search request not found")
 
     def choosing_from_menu(self):
-
+        """
+        The Menu method to enable user input to choose from the different methods of this class
+        @return: None.
+        """
         print('Welcome to your To Do List!!!\n')
-
-
         print(
-            "What would you like to do?\n1.Add New: \n2.Update Existing: \n3.Search something: \n4.Delete: \n5.Show notes\n6.Exit: ")
-        try:
-            input_choice = int(input('Please choose your choice(in no.s): '))
-        except:
-            input_choice = int(input("Sorry,wrong choice. Enter numbers only: "))
-        finally:
-            while True:
-                if input_choice == 1:
-                    while True:
-                        try:
-                            note = input("Add note or press 'E' to exit: ")
-                            if note != 'E':
-                                self.add_notes(note)
-                            else:
+            "What would you like to do?\n1.Add New: \n2.Update Existing: \n3.Search something: \n4.Delete: \n5.Show "
+            "notes\n6.Exit: ")
+        while True:
+            try:
+                input_choice = int(input('Please choose your choice(in no.s): '))
+                if input_choice > 6:
+                    raise Exception
+            except ValueError:
+                print("Sorry,wrong choice. Enter integers only: ")
+            except Exception:
+                print("Please enter numbers in range(1-6) only")
+            else:
+                while True:
+                    if input_choice == 1:
+                        while True:
+                            try:
+                                note = input("Add note or press 'E' to exit: ")
+                                if note != 'E':
+                                    print(self.add_notes(note))
+                                else:
+                                    break
+                            except EOFError:
                                 break
-                        except EOFError:
+                        self.replay()
+
+                    elif input_choice == 2:
+                        edit_notes_index = int(input('Choose which index you want to edit: '))
+                        edit = input('Type your updated note: ')
+                        print(self.__setitem__(edit_notes_index, edit))
+                        self.replay()
+
+                    elif input_choice == 3:
+                        query = input('Search: ')
+                        self.search(query)
+                        self.replay()
+
+                    elif input_choice == 4:
+                        del_index = int(input('Choose which index you want to delete: '))
+                        self.__delitem__(del_index)
+                        print(self)
+                        self.replay()
+
+                    elif input_choice == 5:
+                        print(self, end='')
+                        self.replay()
+
+                    elif input_choice == 6:
+                        exit(0)
+
+                    else:
+                        if not self.replay():
                             break
-                    print(f"TODO item is :\n{self}")
-                    self.replay()
-
-                elif input_choice == 2:
-                    edit_notes_index = int(input('Choose which index you want to edit: '))
-                    edit = input('Type your updated note: ')
-                    self.__setitem__(edit_notes_index, edit)
-                    print(self)
-                    self.replay()
-
-                elif input_choice == 3:
-                    query = input('Search: ')
-                    self.search(query)
-                    self.replay()
-
-                elif input_choice == 4:
-                    del_index = int(input('Choose which index you want to delete: '))
-                    self.__delitem__(del_index)
-                    print(self)
-                    self.replay()
-
-                elif input_choice == 5:
-                    print(self, end='')
-                    self.replay()
-
-                elif input_choice == 6:
-                    exit(0)
-
-                else:
-                    if not self.replay():
-                        break
-
+                break
 
     def replay(self):
+        """
+        Replay method to keep running the application till user decides to exit.
+        @return: empty string
+        """
         while True:
             choice = input('Do you want to do something more(y/n): ')
             if choice.capitalize() == 'Y' or choice.capitalize() == 'N':
@@ -127,13 +198,5 @@ Created at:{self.created_at}\t\t\tModified at:{self.modified_at}
             break
         return ''
 
-
-if __name__ == "__main__":
-    m= To_do_Item()
-    input_title = input('How would you like to name your notes: ')
-    m.add_title(input_title)
-    m.choosing_from_menu()
-
-
-
-
+# TODo : doc string - Done, date in proper format- Done, handling exception- Done,
+# TODO : single method to perform multiple functions, if input_choice>6 error message- Done
